b7eae5cc05fef476600a52fb2b91729c27fee777
Nathan Tebbs <nathan.tebbs2004@gmail.com>
2025-01-05 10:41:56 -0700

    Update tui package contents

--- tui/go.mod
+++ tui/go.mod
@@ -1,3 +1,8 @@
 module github.com/nathantebbs/gpac/tui
 
 go 1.23.4
+
+require (
+	github.com/nathantebbs/gpac/calculator v0.0.1
+	github.com/nathantebbs/gpac/student v0.0.1
+)
--- tui/tui.go
+++ tui/tui.go
@@ -2,8 +2,12 @@ package tui
 
 import (
 	"fmt"
+	"github.com/nathantebbs/gpac/calculator"
+	"github.com/nathantebbs/gpac/student"
 )
 
 func init() {
 	fmt.Println("Hello, Tui!")
+	calculator.init()
+	student.init()
 }
